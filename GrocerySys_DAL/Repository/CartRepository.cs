03f79b034e3c22723217a776df2356e2a5cfcdea
Siddant <Siddant@SIDDANT>
2024-02-20 20:45:46 +0530

    Add project files.

---
+++ GrocerySys_DAL/Repository/CartRepository.cs
@@ -0,0 +1,50 @@
+﻿using GrocerySys_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace GrocerySys_DAL.Repository
+{
+    public class CartRepository
+    {
+        public void addItems(string CustomerID , string ProductID)
+        {
+            using(SqlConnection con = clsConnectionDB.openConnection())
+            {
+                SqlCommand cmd = new SqlCommand("Proc_insertCartItems", con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@customerID", CustomerID);
+                cmd.Parameters.AddWithValue("@productID", ProductID);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public List<getCartModel> getCart()
+        {
+            List<getCartModel> cartList = new List<getCartModel>();
+            using (SqlConnection conn = clsConnectionDB.openConnection())
+            {
+                SqlCommand cmd = new SqlCommand("Proc_getCart" , conn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                using(SqlDataReader SDR = cmd.ExecuteReader())
+                {
+                    while(SDR.Read())
+                    {
+                        getCartModel cart = new getCartModel();
+                        cart.CartID = Convert.ToInt32(SDR["CartID"]);
+                        cart.UserName = SDR["UserName"].ToString();
+                        cart.UserAddress = SDR["UserAddress"].ToString();
+                        cart.ProductName = SDR["ProductName"].ToString();
+                        cart.Quantity = Convert.ToInt32(SDR["Quantity"]);
+                        cart.Price = Convert.ToInt32(SDR["Price"]);
+                        cart.Total = Convert.ToInt32(SDR["Total"]);
+                        cartList.Add(cart); 
+                    }
+                }
+            }
+            return cartList;
+        }
+    }
+}
---
+++ GrocerySys_MVC/Models/getCartModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GrocerySys_MVC.Models
+{
+    public class getCartModel
+    {
+        public int CartID { get; set; }
+        public string UserName { get; set; }
+        public string UserAddress { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+        public int Total { get; set; }
+    }
+}
